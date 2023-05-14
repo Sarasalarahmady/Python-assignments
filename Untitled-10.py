da7adffdf4c96dc1930d081fe68b9ef00d9c3fb6
Sarasalarahmady <132248404+Sarasalarahmady@users.noreply.github.com>
2023-05-14 09:49:32 -0700

    Add files via upload

---
+++ Untitled-10.py
@@ -0,0 +1,14 @@
+import math
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+dalta = b * b - 4 * a * c
+if dalta==0:
+    x=(-b + math.sqrt(delta))
+    print("your answer ,"   X=",x)
+elif delta<0:
+    print("javab haghighi nadarad.")
+else:
+    x1(-b + math.sqrt(delta))/(2*a)
+    x2(-b -math.sqrt(delta))/(2*a)
+    print("do javab darad.\nx1 =",x1, "\nx2=, x2)
